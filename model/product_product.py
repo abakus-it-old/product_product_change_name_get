7f28b201dcff76f37052c90670c484f8147d8dce
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-02 15:21:37 +0200

    Description without reference

--- model/product_product.py
+++ model/product_product.py
@@ -1,4 +1,4 @@
-from openerp import SUPERUSER_ID
+﻿from openerp import SUPERUSER_ID
 from openerp import models, fields, api
 
 class product_product_brand(models.Model):
@@ -64,4 +64,62 @@ class product_product_brand(models.Model):
                           'default_code': product.default_code,
                           }
                 result.append(_name_get(mydict))
+        return result
+
+    def name_get_without_reference(self, cr, user, ids, context=None):
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if not len(ids):
+            return []
+
+        def _name_get_without_reference(d):
+            name = d.get('name','')
+            brand = d.get('brand','')
+            if brand:
+                name = '%s - %s' % (brand.upper(),name)
+            else:
+                name = '%s' % (name)
+            return (d['id'], name)
+
+        partner_id = context.get('partner_id', False)
+        if partner_id:
+            partner_ids = [partner_id, self.pool['res.partner'].browse(cr, user, partner_id, context=context).commercial_partner_id.id]
+        else:
+            partner_ids = []
+
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        self.check_access_rights(cr, user, "read")
+        self.check_access_rule(cr, user, ids, "read", context=context)
+
+        result = []
+                
+        for product in self.browse(cr, SUPERUSER_ID, ids, context=context):
+            variant = ", ".join([v.name for v in product.attribute_value_ids])
+            name = variant and "%s (%s)" % (product.name, variant) or product.name
+            sellers = []
+            if partner_ids:
+                sellers = filter(lambda x: x.name.id in partner_ids, product.seller_ids)
+            if sellers:
+                for s in sellers:
+                    seller_variant = s.product_name and (
+                        variant and "%s (%s)" % (s.product_name, variant) or s.product_name
+                        ) or False
+                    mydict = {
+                              'id': product.id,
+                              'brand': product.product_brand_id.name,
+                              'name': seller_variant or name,
+                              'default_code': s.product_code or product.default_code,
+                              }
+                    result.append(_name_get_without_reference(mydict))
+            else:
+                mydict = {
+                          'id': product.id,
+                          'brand': product.product_brand_id.name,
+                          'name': name,
+                          'default_code': product.default_code,
+                          }
+                result.append(_name_get_without_reference(mydict))
         return result
---
+++ model/sale_order_line.py
@@ -0,0 +1,27 @@
+﻿from openerp.osv import fields, osv
+
+class sale_order_line(osv.osv):
+    _inherit='sale.order.line'
+
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+        uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+        lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
+        
+        result = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty,
+            uom, qty_uos, uos, name, partner_id,
+            lang, update_tax, date_order, packaging, fiscal_position, flag, context)
+        
+        product_obj = self.pool.get('product.product')
+        partner_obj = self.pool.get('res.partner')
+        partner = partner_obj.browse(cr, uid, partner_id)
+        context_partner = {'lang': partner.lang, 'partner_id': partner_id}
+        product_obj = product_obj.browse(cr, uid, product, context=context_partner)
+        
+        if not flag:
+            result['value']['name'] = self.pool.get('product.product').name_get_without_reference(cr, uid, [product_obj.id], context=context_partner)[0][1]
+            if product_obj.description_sale:
+                result['value']['name'] += '\n'+product_obj.description_sale
+
+        return result
+
+sale_order_line()
