51fbc365b86597839c37785b878e6f1bd2a6c378
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-02 15:24:12 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -5,6 +5,7 @@
     "category": 'Sale',
     "description": """
 This modules adds the brand on the lines on sale order when a brand is set.
+It removes the reference of the description in the sale order lines.
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
@@ -13,4 +14,3 @@ This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, 
         'sale','product_brand',
         ],
 }
- 
