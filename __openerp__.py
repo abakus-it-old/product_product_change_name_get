df530e2487e9a2a6f14a8ea582cd626d5edf0cb0
Valentin Thirion <valentinthirion@gmail.com>
2016-04-05 10:15:29 +0200

    Improved desc

--- __openerp__.py
+++ __openerp__.py
@@ -4,13 +4,13 @@
     "author" : "Bernard DELHEZ, AbAKUS it-solutions SARL",
     "category": 'Sale',
     "description": """
+Product brand on sale order lines
+
 This modules adds the brand on the lines on sale order when a brand is set.
 It removes the reference of the description in the sale order lines.
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
     'website': "http://www.abakusitsolutions.eu",
-    'depends' : [
-        'sale','product_brand',
-        ],
+    'depends' : ['sale','product_brand',],
 }
