c2324313496feab3e1a721a385ac9fcc7254208d
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-22 10:16:48 +0200

    Check user access rights, read product as admin

--- model/product_product.py
+++ model/product_product.py
@@ -7,23 +7,28 @@ class product_product_brand(models.Model):
     #It is the same method's body but it includes the brand.
     @api.multi
     def name_get(self):
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        cr = self.env.cr
+        uid = self.env.uid
+        self.check_access_rights(cr, uid, "read")
+        self.check_access_rule(cr, uid, ids, "read")
+        
         result = []
-        for product in self:
-            result.append((product.id, "%s" % (self.name_get_full(product))))
+        for product in self.sudo():
+            result.append((product.id, product.name_get_full()))
         return result
 
-    @api.model
-    def name_get_full(self, product):
-        name = product.name
-        if product.product_brand_id:
-            name = product.product_brand_id.name + " - " + name
-        if product.code:
-            name = product.code + " - " + name
-        return name
+    @api.one
+    def name_get_full(self):
+        fullName = ''
+        
+        if self.product_brand_id:
+            fullName += self.product_brand_id.name + ' - '
+        
+        fullName += self.name
+        
+        if self.default_code:
+            fullName += ' [{}]'.format(self.default_code)
 
-    @api.model
-    def name_get_no_code(self, product):
-        name = product.name
-        if product.product_brand_id:
-            name = product.product_brand_id.name + " - " + name
-        return name
+        return fullName
